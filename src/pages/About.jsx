82b27f265bc0198d815d97f00e90e237884f3b05
Akash Thakur <theakashworld56789@gmail.com>
2026-04-12 03:11:09 +0530

    fix vercel deployment

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
-import NewsLetterBox from '../components/NewsLetterBox'
+import NewsletterBox from '../components/NewsletterBox'
 
 function About() {
   return (
@@ -37,7 +37,7 @@ function About() {
                 <p className='text-gray-600'>Our dedicated customer support team is always ready to assist you with any questions or concerns.</p>
            </div>
       </div>
-      <NewsLetterBox/>
+      <NewsletterBox/>
 
     </div>
   )
